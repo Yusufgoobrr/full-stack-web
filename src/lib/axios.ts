3962862366b360af4b4940e31cd129389da8cad0
mama-samba-braima <djalonelson@gmail.com>
2025-10-24 17:28:07 +0100

    fix: added proxy to allow https to http

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,21 @@
 import axios from "axios";
 
+const resolveBaseUrl = (): string => {
+  const isServer = typeof window === "undefined";
+  if (isServer) {
+    return (
+      process.env.API_BASE_URL ?? "http://product.amigoscodeplayground.com:8080"
+    );
+  }
+
+  if (process.env.NODE_ENV === "production") {
+    return "/api/proxy";
+  }
+
+  return process.env.NEXT_PUBLIC_API_BASE_URL ?? "http://localhost:8080";
+};
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8080",
+  baseURL: resolveBaseUrl(),
   // You can add more default config here
 });
 
